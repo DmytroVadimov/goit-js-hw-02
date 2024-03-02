dcd864b058406e0cdd331a8537cf3554cfdef162
Dmytro Vadimov <104729263+DmytroVadimov@users.noreply.github.com>
2024-03-02 14:50:04 +0200

    task-1

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,1 @@
-"use strict";
-//let content;
-//let padding;
-//let border;
-function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
-    return content + padding * 2 + border * 2
-}
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
-//console.log(getElementWidth("350.1px", "18.1px", "2.2px"));
-
+'use strict';
